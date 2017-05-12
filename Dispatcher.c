90db40fc95842824a257ffa200e64ff6e7c915ea
Jonathan Somer <somer@mail.tau.ac.il>
2017-05-12 20:01:35 +0300

    c

--- Dispatcher.c
+++ Dispatcher.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 
+#include "Dispatcher.h"
 
 #define DISPATCHER_NUM_ARGUMENTS 3
 
@@ -19,11 +20,17 @@ int main(int argc, char **argv) {
 
 	ssize_t fileSize = getFileSize(argv[2]);
 	ssize_t numCharsPerProcess = getSquareRootOfFileSize(fileSize);
+
+
+
+
+
+	return 1;
 }
 
 ssize_t getFileSize(char *filePath)
 {
-	int fileDescriptor = open(*filePath, O_RDONLY);
+	int fileDescriptor = open(filePath, O_RDONLY);
 	if (fileDescriptor < 0)
 	{
 		printf("Error: could not open file\n");
@@ -37,7 +44,9 @@ ssize_t getFileSize(char *filePath)
 		return -1;
 	}
 	close(fileDescriptor);
-	return fileStat.st_size;
+
+	ssize_t fileSize = fileStat.st_size;
+	return fileSize;
 }
 
 ssize_t getSquareRootOfFileSize(ssize_t fileSize)
